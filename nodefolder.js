1b3c1a168b2ec8e0fc53605d1ba7b55c41068743
roscoe <heiwu805@hotmail.com>
2014-12-04 15:58:54 +0800

    update

--- nodefolder.js
+++ nodefolder.js
@@ -2,7 +2,7 @@
 // 引入模块
 var fs = require("fs"),
     options = process.argv,
-    program = require('./commander/index'),
+    program = require('./commander'),
     html_beautify = require('./html_beautify').style_html,
     input = ""
 
@@ -14,7 +14,6 @@ var fileNumCount = 0,
 program
     .version('0.0.1')
     .option('-v, --version ', 'output the version number')
-    .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
     .parse(process.argv)
 
 // 处理选项
@@ -25,7 +24,7 @@ if (program.readHelp) {
 // 处理输入的路径或文件
 input = process.argv[2]
 if (typeof input === "undefined") {
-    console.log("  - you need to input your path, like 'nf ./', or file like './avalon.carousel.ex1.html'")
+    console.log("  - you need to input a file path, like 'nf ./', or a file name like 'nf ./avalon.carousel.ex1.html'")
 } else{
     if(fs.statSync(input).isDirectory()) { // 参数为路径
         var fileList = []
@@ -75,22 +74,23 @@ function handleFile(file){
     console.log("notes gen \t" + file)
 
     // 读入
-    fs.readFile(file, function(e, data){
+    fs.readFile(file, function(e, htmlText){
         if (e) {
             throw e
         }
 
-        data = data.toString()
+        //转字符串
+        htmlText = htmlText.toString()
 
         // 位置相关
-        var preStartLabel = data.match(/<pre .+>/)[0],
-            preStartLocation = data.indexOf("<pre"),
-            preEndLocation = data.indexOf("</pre>") + 6,
-            codeStartLocation = data.indexOf(preStartLabel) + preStartLabel.length,
-            codeEndLocation = data.indexOf("</pre>")
+        var preStartLabel = htmlText.match(/<pre .+>/)[0],
+            preStartLocation = htmlText.indexOf("<pre"),
+            preEndLocation = htmlText.indexOf("</pre>") + 6,
+            codeStartLocation = htmlText.indexOf(preStartLabel) + preStartLabel.length,
+            codeEndLocation = htmlText.indexOf("</pre>")
 
         // 获取代码内容和html内容
-        var codeContent = data.slice(0, preStartLocation) + data.slice(preEndLocation, data.length)
+        var codeContent = htmlText.slice(0, preStartLocation) + htmlText.slice(preEndLocation, htmlText.length)
 
         // code格式化,替换"<"和">"
         codeContent = html_beautify(codeContent)
@@ -98,15 +98,15 @@ function handleFile(file){
         codeContent = codeContent.replace(/>/g, "&gt;")
 
         // 把注释填入pre
-        data = data.slice(0, codeStartLocation) + "\n" + codeContent + "\n" + data.slice(codeEndLocation, data.length)
+        htmlText = htmlText.slice(0, codeStartLocation) + "\n" + codeContent + "\n" + htmlText.slice(codeEndLocation, htmlText.length)
 
         // 格式化Html
-        data = html_beautify(data, {
+        htmlText = html_beautify(htmlText, {
             unformatted: ['pre']
         })
 
         // 写入
-        fs.writeFile(file, data, function(e){
+        fs.writeFile(file, htmlText, function(e){
             if(e) {
                 throw e
             }
